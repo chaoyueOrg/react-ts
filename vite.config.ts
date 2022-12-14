91522ab641d8894a26f3b6fb1e50b72964e0a10a
johnnyXue <johnny@interactivelabs.co>
2022-12-14 11:01:20 +0800

    chore: alias config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  css: {
+    modules: {
+      generateScopedName: "[local]-[hash]",
+      localsConvention: "camelCase",
+    },
+  },
+  server: {
+    hmr: true,
+  },
+});
